e9ab07806f5eb305eec46361bdcf5571710b1d44
ArturTan <artur.tanona@gmail.com>
2017-07-16 20:11:38 +0200

    Update start.py

--- start.py
+++ start.py
@@ -3,11 +3,11 @@ import grot
 if __name__ == "__main__":
     
     side_length = int(input("What is side of your sample?:   "))
-    sample, sample2 = sample_generator(side_length)
+    sample, sample2 = grot.sample_generator(side_length)
     o = range(len(sample[0]))
     chains = [] # here we put all chains that are in our sample
-    grot_search = GrotSearcher(sample, chains, o)
-    chains = grot_search.backend()
+    grot_search = grot.GrotSearcher(sample, chains, o)
+    chains = grot.grot_search.backend()
     
-    max_lens = Viewer.max_lens(chains)
-    Viewer.shower(max_lens, chains, sample2)
+    max_lens = grot.max_lens(chains)
+    grot.hower(max_lens, chains, sample2)
